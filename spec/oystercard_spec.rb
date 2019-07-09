d95d9e2875f574606952ea406157a68d739dc21e
Guilherme <guiheurich@gmail.com>
2019-07-09 10:50:45 +0100

    Implement user story 2
    In order to keep using public transport
    As a customer
    I want to add money to my card

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,16 +1,28 @@
 require 'oystercard'
 
 describe Oystercard do
-  # In order to use public transport
-  # As a customer
-  # I want money on my card
+
+  subject(:oystercard) { described_class.new }
+
   describe '#balance' do
     context 'when a user needs to know its balance' do
       it 'checks for the balance' do
-        oystercard = Oystercard.new
         expect(oystercard).to respond_to(:balance)
         expect(oystercard.balance).to eq 0
       end
     end
   end
+
+  # In order to keep using public transport
+  # As a customer
+  # I want to add money to my card
+  describe '#top_up' do
+    context 'when a user needs to use public transport' do
+      it 'adds money to a card' do
+        expect(oystercard).to respond_to(:top_up).with(1).argument
+        expect(oystercard.top_up(20)).to eq(20)
+      end
+    end
+  end
+
 end
